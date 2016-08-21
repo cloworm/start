a86e472664381de820f398af06058ba19f0cec76
cloworm <chloe.echikson@gmail.com>
2016-08-20 20:17:13 -0400

    Numbers automatically update when days are deleted

--- public/js/days.js
+++ public/js/days.js
@@ -1,41 +1,27 @@
 var dayModule = (function() {
-  var numDays = 1;
   var $currentDay = $('.current-day');
 
-  // Add a new day
-  $('.add-day').on('click', function() {
-    addNewDay(numDays);
-  })
+  // Store each day's itinerary in an object
+  var Day = function() {
+    this.hotel = '';
+    this.restaurants = [];
+    this.activities = [];
+  }
 
-  // Switch days
-  $('#days').on('click', '.day-buttons', function() {
-    setCurrentDay($(this))
-  })
-
-  // Remove a day
-  $('.remove-day').on('click', function() {
-    const MIN_DAYS = 1;
-
-    if (numDays > MIN_DAYS && $currentDay.text !== '1') {
-      // get current day number
-      // get previous day number
-      $prevDay = $currentDay.prev();
-      // remove current day class from current day
-      setCurrentDay($prevDay);
-      // add current day class to previous day number
-      // remove current day
-      deleteDay($currentDay);
-    }
-  })
+  var days = [new Day()];
 
   function addNewDay(currentNumDays) {
-    numDays++;
-    $('.add-day').before(`<a class="btn-floating white day-buttons teal-text">${currentNumDays + 1}</a>`)
+    days.push(new Day());
+    renderDay(currentNumDays + 1);
   }
 
   function setCurrentDay(day) {
     $currentDay.removeClass('current-day');
     day.addClass('current-day');
+
+    // Change title of days itinerary
+    var $currentTitle = $('#day-title')
+    $currentTitle[0].innerText = $currentTitle[0].innerText.replace(/\d+/, day[0].text);
 
     // redefine current day
     $currentDay = $('.current-day');
@@ -43,6 +29,46 @@ var dayModule = (function() {
 
   function deleteDay(day) {
     day.remove();
+
+    // remove from days array
+    days.splice(day.text - 1, 1);
   }
+
+  function renderDay(dayNumber) {
+    $('#day-buttons').append(`<a class="btn-floating white day-button teal-text">${dayNumber}</a>`)
+  }
+
+  // Clicking new day button
+  $('.add-day').on('click', function() {
+    addNewDay(days.length);
+  })
+
+  // Clicking on a day button
+  $('#days').on('click', '.day-button', function() {
+    setCurrentDay($(this))
+  })
+
+  // Clicking remove day button
+  $('.remove-day').on('click', function() {
+    const MIN_DAYS = 1;
+    var currentDayNum = Number($currentDay[0].text);
+
+    if (days.length > MIN_DAYS && currentDayNum !== 1) {
+      // delete current day button
+      deleteDay($currentDay);
+
+      // renumber all days
+      $('#day-buttons').html('')
+      days.forEach(function(day, index) {
+        renderDay(index + 1);
+      });
+
+      // set class to stored current day
+      var prevDay = currentDayNum - 1
+      var $prevDay = $($('.day-button')[prevDay - 1]);
+      $prevDay.addClass('current-day');
+      $currentDay = $prevDay;
+    }
+  })
 
 })();
